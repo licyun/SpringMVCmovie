cdf32975e87726ca62aa6d68017dd6a6881bf8ac
mzkwy <mzkwy@outlook.com>
2016-10-19 18:15:24 +0800

    fixed sql

--- movie.sql
+++ movie.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `user`
 --
 
+CREATE DATABASE movie DEFAULT CHARACTER SET utf8;
+
+USE movie;
+
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
